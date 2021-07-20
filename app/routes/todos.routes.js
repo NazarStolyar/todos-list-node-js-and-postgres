921435b8505e1a8adf0aeb0f62b8e88cd2833ea3
Nazar <nazar@MacBook-Pro-admin-2.local>
2021-07-20 15:25:35 +0300

    edit

--- app/routes/todos.routes.js
+++ app/routes/todos.routes.js
@@ -4,5 +4,6 @@ const TodosController = require('../controller/todos.controller');
 const AuthMiddleware = require('../middleware/auth.middlewear')
 
 router.post(`/create`, AuthMiddleware, TodosController.createTodos)
+router.post(`/edit/:id`, AuthMiddleware, TodosController.updateTodos)
 
 module.exports = router;
