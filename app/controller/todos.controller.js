5ff5979528b9ecbec664d408c7da27f5496f6d49
Nazar <nazar@MacBook-Pro-admin-2.local>
2021-07-20 23:26:56 +0300

    search

--- app/controller/todos.controller.js
+++ app/controller/todos.controller.js
@@ -1,8 +1,8 @@
-const bd = require('../../db')
+const db = require('../../db')
 class TodosController {
     async createTodos(req, res) {
         const { title, description, date, user_id, status, type, is_completed } = req.body
-        const newTodos = await bd.query(`INSERT INTO todos (title, description, date, user_id, status, type, is_completed) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, [title, description, date, user_id, status, type, is_completed])
+        const newTodos = await db.query(`INSERT INTO todos (title, description, date, user_id, status, type, is_completed) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, [title, description, date, user_id, status, type, is_completed])
         try {
             res.json({
                 status: 'success',
@@ -15,10 +15,43 @@ class TodosController {
             })
         }
     }
-    async getTodo(req, res) {}
-    async getTodosByUserId(req, res) {}
+    async getTodoById(req, res) {
+        const id = req.params.id;
+        const todo = await db.query(`SELECT * FROM todos where id = $1`, [id])
+        try {
+            res.json({
+                status: 'success',
+                data: todo.rows[0]
+            })
+        } catch (e) {
+            res.json({
+                status: 'failed',
+                message: 'System error'
+            })
+        }
+    }
+    async getTodosByUserId(req, res) {
+        const { id } = req.params;
+        const todos = await db.query(`SELECT * FROM todos where user_id = $1`, [id])
+        try {
+            res.json({
+                status: 'success',
+                data: todos.rows
+            })
+        } catch (e) {
+            res.json({
+                status: 'failed',
+                message: 'System error'
+            })
+        }
+    }
     async updateTodos(req, res) {}
     async deleteTodos(req, res) {}
+    async searchTodos(req, res, next) {
+        const value = 'one'
+        const todos = await db.query(`SELECT * FROM todos where to_tsvector(title) @@ to_tsquery($1)`, [value])
+        console.log(todos.rows)
+    }
 }
 
 module.exports = new TodosController()
--- app/routes/todos.routes.js
+++ app/routes/todos.routes.js
@@ -4,6 +4,12 @@ const TodosController = require('../controller/todos.controller');
 const AuthMiddleware = require('../middleware/auth.middlewear')
 
 router.post(`/create`, AuthMiddleware, TodosController.createTodos)
-router.post(`/edit/:id`, AuthMiddleware, TodosController.updateTodos)
+router.put(`/edit/:id`, AuthMiddleware, TodosController.updateTodos)
+router.delete(`/delete/:id`, AuthMiddleware, TodosController.deleteTodos)
+router.get(`/:id`, AuthMiddleware, TodosController.getTodoById)
+router.get(`/user_id/:id`, AuthMiddleware, TodosController.getTodosByUserId)
+router.get(`/`, AuthMiddleware, TodosController.searchTodos)
 
 module.exports = router;
+
+
